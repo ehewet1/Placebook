53b2a442b58ed03a86af6f3809febb0e2a0f6d58
ehewet1 <ehewett32425@live.com>
2021-09-19 12:18:21 -0500

    End of Ch17

--- app/src/main/java/com/raywenderlich/placebook/adapter/BookmarkInfoWindowAdapter.kt
+++ app/src/main/java/com/raywenderlich/placebook/adapter/BookmarkInfoWindowAdapter.kt
@@ -6,8 +6,9 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.Marker
 import com.raywenderlich.placebook.databinding.ContentBookmarkInfoBinding
 import com.raywenderlich.placebook.ui.MapsActivity
+import com.raywenderlich.placebook.viewmodel.MapsViewModel
 
-class BookmarkInfoWindowAdapter(context: Activity) : GoogleMap.InfoWindowAdapter {
+class BookmarkInfoWindowAdapter(val context: Activity) : GoogleMap.InfoWindowAdapter {
 
     private val binding = ContentBookmarkInfoBinding.inflate(context.layoutInflater)
 
@@ -18,11 +19,19 @@ class BookmarkInfoWindowAdapter(context: Activity) : GoogleMap.InfoWindowAdapter
     override fun getInfoContents(marker: Marker): View? {
         binding.title.text = marker.title ?: ""
         binding.phone.text = marker.snippet ?: ""
+
         val imageView=binding.photo
-
-        imageView.setImageBitmap((marker.tag as
-            MapsActivity.PlaceInfo).image)
-
+        when(marker.tag){
+            is MapsActivity.PlaceInfo ->{
+                imageView.setImageBitmap(
+                    (marker.tag as MapsActivity.PlaceInfo).image)
+            }
+            is MapsViewModel.BookmarkMarkerView ->{
+                val bookMarkview = marker.tag as
+                  MapsViewModel.BookmarkMarkerView
+                imageView.setImageBitmap(bookMarkview.getImage(context))
+            }
+        }
         return binding.root
     }
 }
--- app/src/main/java/com/raywenderlich/placebook/ui/MapsActivity.kt
+++ app/src/main/java/com/raywenderlich/placebook/ui/MapsActivity.kt
@@ -2,6 +2,7 @@ package com.raywenderlich.placebook.ui
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
@@ -142,6 +143,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             .snippet(place.phoneNumber)
         )
         marker?.tag=PlaceInfo(place,photo)
+        marker?.showInfoWindow()
     }
 
     @SuppressLint("MissingSuperCall")
@@ -164,13 +166,25 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     private fun handleInfoWindowClick(marker: Marker){
-        val placeInfo=(marker.tag as PlaceInfo)
-        if (placeInfo.place!=null){
-            GlobalScope.launch {
-                mapsViewModel.addBookmarkFromPlace(placeInfo.place,placeInfo.image)
+        when (marker.tag){
+            is PlaceInfo -> {
+                val placeInfo = (marker.tag as PlaceInfo)
+                if (placeInfo.place != null && placeInfo.image != null) {
+                    GlobalScope.launch {
+                        mapsViewModel.addBookmarkFromPlace(placeInfo.place, placeInfo.image)
+                    }
+                }
+                marker.remove()
+            }
+            is MapsViewModel.BookmarkMarkerView->{
+                val bookmarkMarkerView=(marker.tag as
+                        MapsViewModel.BookmarkMarkerView)
+                marker.hideInfoWindow()
+                bookmarkMarkerView.id?.let{
+                    startBookmarkDetails(it)
+                }
             }
         }
-        marker.remove()
     }
 
     private fun createBookmarkMarkerObserver(){
@@ -194,6 +208,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             bookmark:MapsViewModel.BookmarkMarkerView):Marker?{
         val marker=map.addMarker(MarkerOptions()
             .position(bookmark.location)
+            .title(bookmark.name)
+            .snippet(bookmark.phone)
             .icon(BitmapDescriptorFactory.defaultMarker(
                 BitmapDescriptorFactory.HUE_AZURE))
             .alpha(0.8f))
@@ -230,8 +246,15 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     companion object{
+        const val EXTRA_BOOKMARK_ID="com.rayewenderlich.placebook.EXTRA_BOOKMARK_ID"
         private const val REQUEST_LOCATION = 1
         private const val TAG="MapsActivity"
+    }
+
+    private fun startBookmarkDetails(bookmarkId: Long){
+        val intent= Intent(this, BookmarkDetailsActivity::class.java)
+        intent.putExtra(EXTRA_BOOKMARK_ID, bookmarkId)
+        startActivity(intent)
     }
 
     class PlaceInfo(val place: Place?=null, val image: Bitmap?=null)
